e4bf870c6eb73562081f602c7b337294b146767c
Евгений Желябовский (h) <zhelyabovskij.eg@reliab.tech>
2022-08-29 20:20:37 +0300

    hw02

--- src/main/java/com/EZhInfo/PomodoroTimer.java
+++ src/main/java/com/EZhInfo/PomodoroTimer.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class PomodoroTimer {
 
     /**
-     * input: -w -b  -время работы  -время отдыха
+     * input: -w -b -c -m  -время работы  -время отдыха -количество повторов -множитель
      * output: Таймер заведен. Пора работать
      * истекла минута
      * output: пора отдыхать
@@ -17,25 +17,35 @@ public class PomodoroTimer {
      */
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Введите время работы и отдыха");
-        // [-w, 1, -b, 1]
+        // [-w 1, -b 1, -count 1, -m 1]
         var cmd = new Scanner(System.in).nextLine().split(" ");
 
         int workTime = 1;
         int breakTime = 1;
+        int count = 1;
+        int mult = 1;
         boolean isHelp = false;
 
         for (int i = 0; i < cmd.length; i++) {
             switch (cmd[i]) {
                 case "-w" -> workTime = Integer.parseInt(cmd[++i]);
                 case "-b" -> breakTime = Integer.parseInt(cmd[++i]);
+                case "-c" -> count = Integer.parseInt(cmd[++i]);
+                case "-m" -> mult = Integer.parseInt(cmd[++i]);
                 case "--help" -> {
                     System.out.println("""
                             \nPromodo - это приложение для улучшения личной эффективности труда.
                             Параметры команды:
                             -w - время работы, минут
                             -b - время отдыха, минут
+                            -c - количество интераций для времени работы
+                            -m - множитель рабочего времени
                             --help - вызов справки
                             """);
+                    isHelp = true;
+                }
+                default -> {
+                    System.out.println("Неправильно заданы параметры");
                     isHelp = true;
                 }
             }
@@ -44,25 +54,27 @@ public class PomodoroTimer {
         if (isHelp) return;
 
         long startTime = System.currentTimeMillis();
-        timer(workTime, breakTime);
+        timer(workTime, breakTime, count, mult);
         long stopTime = System.currentTimeMillis();
         System.out.println("Таймер работал " + (stopTime - startTime) / (1000 * 60) + " мин.");
-
-        System.out.println("workTime = " + workTime + ", breakTime = " + breakTime);
-
     }
 
-    public static void timer(int workTime, int breakTime) throws InterruptedException {
-        printProgress("Work progress: ", workTime, 30);
-        printProgress("Break progress: ", breakTime, 30);
+    public static void timer(int workTime, int breakTime, int count, int mult) throws InterruptedException {
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Подход к работе №" + i);
+            printProgress("Вкалываем: ", workTime, 30);
+            workTime *= mult;
+            printProgress("Релакс: ", breakTime, 30);
+        }
     }
-    private static void printProgress(String process, int time, int size) throws InterruptedException {
+
+    private static void printProgress(String process, int time, int sizeBar) throws InterruptedException {
         int length;
         int rep;
 
-        length = 60 * time / size;
+        length = 60 * time / sizeBar;
         rep = 60 * time / length;
-        int strechb = size / (3 * time);
+        int strechb = sizeBar / (3 * time);
 
         for (int i =1; i <= rep; i++){
             double x = i;
